92cb8ef18bb2ea77fa46df34ac061862eee7f513
cbsksbx <cbsksbx@gmail.com>
2026-03-19 20:00:01 +0900

    Update task 04.3.py

--- task 04.3.py
+++ task 04.3.py
@@ -2,7 +2,7 @@
 #3) 원의 면적, input 이용, math 이용
 import math
 r = int(input("원의 반지름을 입력하시오"))
-area = round((math.pi * r**2), 2)
-perimeter = round((math.pi * r), 1)
+area = round((math.pi * r**2), 1)
+perimeter = round((math.pi * r), 2)
 print(f"원의 면적은 {area}㎠이고 둘레는{perimeter}㎠입니다.")
 print("원의 면적은 {}㎠이고 둘레는{}㎠입니다.".format(area, perimeter))
